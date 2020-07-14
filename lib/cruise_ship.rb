98a4cf602518d2006169be408ca4dfdedd5501bd
Sanam Malik <sanam.malik04@gmail.com>
2020-07-14 18:32:10 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -25,4 +25,7 @@ birthday_kids = {
 
 def happy_birthday (birthday_kids)
   birthday_kids.each do |kids_name, age|
-    
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+
